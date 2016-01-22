3c54ab645617c1ce701d9fd961a05ba57869e7fe
adam <adam@DESKTOP-9S0171J>
2016-01-22 16:45:01 +0900

    train

--- src/net/tokyo_ct/meister2015/jellyfish/train/Train.java
+++ src/net/tokyo_ct/meister2015/jellyfish/train/Train.java
@@ -2,22 +2,44 @@ package net.tokyo_ct.meister2015.jellyfish.train;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Train {
-	URL searchURL;
-	String staName;
-	
-	private Train() {
-		try {
-			this.searchURL = new URL("http://www.ekikara.jp/cgi-bin/find.cgi");
-		} catch (MalformedURLException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
+	String data;
+
+	public Train(String data) {
+		this.data = data;
+	}
+
+	public String getTrainsFromDay(String day) {
+		System.out.println(data);
+		Matcher m1 = Pattern.compile(".*\\[" + day + "\\].*", Pattern.MULTILINE).matcher(data);
+		Matcher m2 = Pattern.compile("H\\sI", Pattern.MULTILINE).matcher(data);
+
+		String str = null;
+		int start = 0;
+		int end = 0;
+		if (m1.find()) {
+			start = m1.start();
 		}
+
+		m2.region(start, data.length());
+		if (m2.find()) {
+			end = m2.end();
+		}
+		data = data.substring(start, end);
+		return data;
 	}
-	
-	public Train(String staName){
-		this();
-		this.staName=staName;
+
+	public String[] getTrainsFromHour(String hour) {
+		Matcher m1 = Pattern.compile("^" + hour + ".*$", Pattern.MULTILINE).matcher(data);
+		String str = "";
+		if (m1.find()) {
+			str = m1.group(0);
+		}
+		return str.split(":| ");
+
 	}
+
 }
