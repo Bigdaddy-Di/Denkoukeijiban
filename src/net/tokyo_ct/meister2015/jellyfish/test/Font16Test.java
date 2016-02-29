0a3178553037137ead9530e2ac6618d1effab65a
TeamET <TeamET@172.18.206.210>
2016-02-29 19:32:21 +0900

    aaa

--- src/net/tokyo_ct/meister2015/jellyfish/test/Font16Test.java
+++ src/net/tokyo_ct/meister2015/jellyfish/test/Font16Test.java
@@ -7,14 +7,37 @@ import net.tokyo_ct.meister2015.jellyfish.font.Font16;
 public class Font16Test {
 	@Test
 	public void test() throws InterruptedException {
+		// Font16 f = new Font16();
+		// f.str = "あいうえおかきくけこさしすせそ";
+		// f.toImage();
+		// for (int x = 0; x < 16 * 8; x++) {
+		// //print(new Font16(f.toBoolean(x, 0), 16 * 8, 16));
+		// Thread.sleep(100);
+		// }
+		// System.out.println(f.ys);
+
 		Font16 f = new Font16();
-		f.str = "あいうえおかきくけこさしすせそ";
-		f.toImage();
-		for (int x = 0; x < 16 * 8; x++) {
-			//print(new Font16(f.toBoolean(x, 0), 16 * 8, 16));
-			Thread.sleep(100);
+		boolean[][] bit = {
+				{ false, true, false, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, false, true, true, true, true, true, true, true, true, true, false, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, false, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, false, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, false, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, false, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, false, true, true, true, true },
+				{ true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false },
+		};
+		byte[] bits = f.exchange(bit);
+		for (byte b : bits) {
+			System.out.print(b+",");
 		}
-		System.out.println(f.ys);
 	}
 
 	public static void print(Font16 f) {
